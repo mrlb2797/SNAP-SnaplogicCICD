b29de3c34542f5ed714189102cfe85a088d05838
mrlb2797 <101921919+mrlb2797@users.noreply.github.com>
2023-01-21 21:12:15 +0000

    Update SL-RemoveProjectAccountPasswords.py

--- .pythonscripts/SL-RemoveProjectAccountPasswords.py
+++ .pythonscripts/SL-RemoveProjectAccountPasswords.py
@@ -7,20 +7,20 @@ import os
 def Obfuscate_Password(FolderPath, SL_Account_File):
 
     #Reading the SnapLogic Account file
-    SnaplogicAccountFile = open(FolderPath+"/"+SL_Account_File, "r"):
+    SnaplogicAccountFile = open(FolderPath+"/"+SL_Account_File, "r")
 
     #Parse the JSON contents to create
     #a Python Dictionary object
-    SnaplogicAccount_PyDict = json.loads(SnaplogicAccountFile.read()):
-    SnaplogicAccountFile.close():
+    SnaplogicAccount_PyDict = json.loads(SnaplogicAccountFile.read())
+    SnaplogicAccountFile.close()
 
     #Replace property_map.settings.password.value 
-    SnaplogicAccount_PyDict["property_map"]["settings"]["password"]["value"] = "NO       PASSWORD - Removed by GitHub action":
+    SnaplogicAccount_PyDict["property_map"]["settings"]["password"]["value"] = "NO       PASSWORD - Removed by GitHub action"
 
     #Convert the Python Dictionary object back to JSON
     #and overwrite the original file
-    SnaplogicAccountFile = open(FolderPath+"/"+SL_Account_File, "w"):
-    SnaplogicAccountFile.write(json.dumps(SnaplogicAccount_PyDict, indent=4)):
+    SnaplogicAccountFile = open(FolderPath+"/"+SL_Account_File, "w")
+    SnaplogicAccountFile.write(json.dumps(SnaplogicAccount_PyDict, indent=4))
     SnaplogicAccountFile.close()
 
 #Parse script arguments
