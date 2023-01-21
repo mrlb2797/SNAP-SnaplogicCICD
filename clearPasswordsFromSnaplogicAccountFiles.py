7bf6c763d0bfabb3ad57c781023ca1e677fc35ba
mrlb2797 <101921919+mrlb2797@users.noreply.github.com>
2023-01-21 16:10:45 +0000

    Update clearPasswordsFromSnaplogicAccountFiles.py

--- clearPasswordsFromSnaplogicAccountFiles.py
+++ clearPasswordsFromSnaplogicAccountFiles.py
@@ -1,8 +1,18 @@
 # Importing re module
 import json
+import argparse
+
+#Parse script arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--sla_filename", type=str)
+
+#Map arguments to variables
+args = parser.parse_args()
+SnaplogicAccountFileName = args.sla_filename
+
   
 #Reading the SnapLogic Account file
-SnaplogicAccountFile = open("Test IFS Account.sla", "r")
+SnaplogicAccountFile = open(SnaplogicAccountFileName, "r")
 
 #Parse the JSON contents to create
 #a Python Dictionary object
@@ -14,10 +24,10 @@ SnaplogicAccount_PyDict["property_map"]["settings"]["password"]["value"] = "NO  
 
 #Convert the Python Dictionary object back to JSON
 #and overwrite the original file
-SnaplogicAccountFile = open("Test IFS Account.sla", "w")
+SnaplogicAccountFile = open(SnaplogicAccountFileName, "w")
 SnaplogicAccountFile.write(json.dumps(SnaplogicAccount_PyDict, indent=4))
 SnaplogicAccountFile.close()
   
 #open and read the file after the overwriting:
-f = open("Test IFS Account.sla", "r")
+f = open(SnaplogicAccountFileName, "r")
 print(f.read())
