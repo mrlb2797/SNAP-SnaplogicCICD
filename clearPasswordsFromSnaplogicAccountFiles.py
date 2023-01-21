61fc7372fe969c2a18bf7d89fdf93f040def47a8
mrlb2797 <101921919+mrlb2797@users.noreply.github.com>
2023-01-21 15:19:29 +0000

    Update clearPasswordsFromSnaplogicAccountFiles.py

--- clearPasswordsFromSnaplogicAccountFiles.py
+++ clearPasswordsFromSnaplogicAccountFiles.py
@@ -6,9 +6,16 @@ SnaplogicAccountFile = open("Test IFS Account.sla", "r")
 
 #Parse the JSON contents to create
 #a Python Dictionary object
-SnaplogicAccountDictionary = json.loads(SnaplogicAccountFile.read())
+SnaplogicAccount_PyDict = json.loads(SnaplogicAccountFile.read())
+SnaplogicAccountFile.close()
 
 #Replace property_map.settings.password.value 
-SnaplogicAccountDictionary["property_map"]["settings"]["password"]["value"] = "NO       PASSWORD - Removed by GitHub action"
+SnaplogicAccount_PyDict["property_map"]["settings"]["password"]["value"] = "NO       PASSWORD - Removed by GitHub action"
+
+#Convert the Python Dictionary object back to JSON
+#and overwrite the original file
+SnaplogicAccountFile = open("Test IFS Account.sla", "w")
+SnaplogicAccountFile.write(json.dumps(SnaplogicAccount_PyDict, indent=4))
+SnaplogicAccountFile.close()
   
 print(SnaplogicAccountDictionary["property_map"]["settings"]["password"]["value"])
