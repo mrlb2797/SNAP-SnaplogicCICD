c01afff4dfd68b2dc20867a9a31bd651e8b83e1e
mrlb2797 <101921919+mrlb2797@users.noreply.github.com>
2023-01-21 23:11:17 +0000

    Update SL-RemoveProjectAccountPasswords.py

--- .pythonscripts/SL-RemoveProjectAccountPasswords.py
+++ .pythonscripts/SL-RemoveProjectAccountPasswords.py
@@ -14,15 +14,21 @@ def Obfuscate_Password(FolderPath, SL_Account_File):
     SnaplogicAccount_PyDict = json.loads(SnaplogicAccountFile.read())
     SnaplogicAccountFile.close()
 
-    #Replace property_map.settings.password.value 
-    SnaplogicAccount_PyDict["property_map"]["settings"]["password"]["value"] = "NO       PASSWORD - Removed by GitHub action"
+    #Check if the 'value' field has already been updated previously
+    if SnaplogicAccount_PyDict["property_map"]["settings"]["password"]["value"] == "NO       PASSWORD - Removed by GitHub action":
+        return 0
+    else:
+    
+        #Replace property_map.settings.password.value 
+        SnaplogicAccount_PyDict["property_map"]["settings"]["password"]["value"] = "NO       PASSWORD - Removed by GitHub action"
 
-    #Convert the Python Dictionary object back to JSON
-    #and overwrite the original file
-    SnaplogicAccountFile = open(FolderPath+"/"+SL_Account_File, "w")
-    SnaplogicAccountFile.write(json.dumps(SnaplogicAccount_PyDict, indent=4))
-    SnaplogicAccountFile.close()
-    print("Password obsfucated for SL Account:"+SL_Account_File[0:-4])
+        #Convert the Python Dictionary object back to JSON
+        #and overwrite the original file
+        SnaplogicAccountFile = open(FolderPath+"/"+SL_Account_File, "w")
+        SnaplogicAccountFile.write(json.dumps(SnaplogicAccount_PyDict, indent=4))
+        SnaplogicAccountFile.close()
+        print("Password obsfucated for SL Account:"+SL_Account_File[0:-4])
+        return 1
 
 #Parse script arguments
 parser = argparse.ArgumentParser()
@@ -38,9 +44,9 @@ SL_Accounts_Changed = 0
 for SL_ProjectAsset in os.listdir(SL_ProjectFolderPath):
     if SL_ProjectAsset.endswith(".sla"):
         # Prints only text file present in My Folder
-        Obfuscate_Password(SL_ProjectFolderPath,SL_ProjectAsset)
+        increment = Obfuscate_Password(SL_ProjectFolderPath,SL_ProjectAsset)
         # Increment counter
-        SL_Accounts_Changed = SL_Accounts_Changed +1
+        SL_Accounts_Changed = SL_Accounts_Changed + increment
         
 print("Accounts modified: "+str(SL_Accounts_Changed))
 
